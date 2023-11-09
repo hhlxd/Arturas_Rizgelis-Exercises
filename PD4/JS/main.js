0cf333bb94fe39247560ad6ee1f29448f5fcd703
arturas <Arturasrizgelis3@gmail.com>
2023-11-09 15:33:29 +0200

    Work Arturas

---
+++ PD4/JS/main.js
@@ -0,0 +1,15 @@
+let petName;
+let petAge;
+
+petName = "kate";
+petAge = 5;
+
+console.log("Pet Name: " + petName);
+console.log("Pet Age: " + petAge);
+
+petName = "keksas";
+petAge = 7;
+
+const buildingAddress = "Gatve 1";
+
+console.log("Building Address: " + buildingAddress);
