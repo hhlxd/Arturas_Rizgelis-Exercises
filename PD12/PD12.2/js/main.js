17a3ef2da6ed3807b6885845fb6800fb14c7e924
Arturas <144884411+hhlxd@users.noreply.github.com>
2024-01-25 12:56:11 +0200

    Delete PD12 directory

--- PD12/PD12.2/js/main.js
+++
@@ -1,4 +0,0 @@
-let puodeliai=7;
-let dezutes=2;
-
-console
--- PD12/pd13/pd13.1/js/main.js
+++
@@ -1,8 +0,0 @@
-let x = 5;
-let y = 10;
-
-if (x > y) {
-  console.log("Didesnis skaičius: x");
-} else if (x < y) {
-  console.log("Didesnis skaičius: y");
-}
